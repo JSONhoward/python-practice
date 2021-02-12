df22ecac692b25a90525ac69b4453169f261b442
Jason Shaw <yourhoward@gmail.com>
2021-02-11 23:50:16 -0800

    added more add methods to circular linked list

--- src/DataStructures/circular_linked_list.py
+++ src/DataStructures/circular_linked_list.py
@@ -17,6 +17,34 @@ class CircularLinkedList:
             self.head = node
             self.head.next = self.head
             self.size += 1
+        elif self.head == self.head.next:
+            self.head.next = node
+            node.next = self.head
+            self.head = node
+            self.size += 1
+        else:
+            node.next = self.head
+            self.head = node
+
+            current = self.head
+
+            while current.next is not self.head:
+                current = current.next
+
+            current.next = self.head
+            self.size += 1
+
+    def add_at_tail(self, data):
+        node = Node(data)
+
+        if self.head is None:
+            self.head = node
+            self.head.next = self.head
+            self.size += 1
+        elif self.head == self.head.next:
+            node.next = self.head
+            self.head.next = node
+            self.size += 1
         else:
             current = self.head
 
@@ -25,4 +53,26 @@ class CircularLinkedList:
 
             current.next = node
             node.next = self.head
+            self.size += 1
+
+    def add_after(self, node_data, data):
+        node = Node(data)
+
+        if self.head is None:
+            return False
+        elif self.head == self.head.next and self.head.data == node_data:
+            node.next = self.head
+            self.head.next = node
+            self.size += 1
+        else:
+            current = self.head
+
+            while current.data != node_data and current.next is not self.head:
+                current = current.next
+
+            if current.data != node_data:
+                return False
+            
+            node.next = current.next
+            current.next = node
             self.size += 1
